b6c47315f8c87a67257b0dae5b75956a63867534
Assasin238238 <harnach2006@gmail.com>
2024-11-23 23:51:20 +0100

    restrukturalizace komplet stránky; folders, subpages, basic css, atd; version: 0.010

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Minesweeper Game</title>
+</head>
+<body>
+<header>
+        <div class="navbar">
+            <div class="logo"><a href="#" title="Přejít na hlavní stránku">Minesweeper Game</a></div>
+            <ul class="links">
+                <!-- Navigation links -->
+                <li><a href="subpages/minesweeper.php">Game</a></li>
+            </ul>
+            <!-- Privacy action button -->
+            <a href="subpages/registration.php" class="action_btn">Sign up</a>
+            <a href="subpages/login.php" class="action_btn">Log in</a>
+            <!-- Toggle button for responsive menu -->
+            <div class="toggle_btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+
+    </header>
+    <main>
+        <section id="uvod">
+            <h1>Welcome</h1>
+            <!-- Button to start playing the game -->
+            <a href="subpages/minesweeper.php" class="action_btn" id="playBTN">Play</a>
+        </section>
+    </main>
+
+<?php include "php/footer.php" ?>
+</body>
+</html>
