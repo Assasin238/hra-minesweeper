c85fa3eae3acd6e9057c37959e4eaa79fdaa47a7
Assasin238238 <harnach2006@gmail.com>
2024-12-10 13:19:13 +0100

    vysvětlivky k DB;

--- subpages/database.php
+++ subpages/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostName = "localhost";
-$dbUser = "harnachp";
-$dbPassword = "";
-$dbName = "harnachp_login_register";
+$dbUser = "root"; //max přepsat na jméno u db
+$dbPassword = ""; //přepsat když u vyšinky
+$dbName = "harnachp_login_register"; //jméno db pak je users pro kolonky
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong:");
