7e8b5924b9e1ce8d44477704a3e2d1c2e3b37547
Assasin238238 <harnach2006@gmail.com>
2025-01-03 18:25:41 +0100

    další comments; version: 0.031

--- subpages/database.php
+++ subpages/database.php
@@ -3,7 +3,7 @@
 $hostName = "localhost";
 $dbUser = "root"; //max přepsat na jméno u db
 $dbPassword = ""; //přepsat když u vyšinky
-$dbName = "harnachp_login_register"; //jméno db, pak je users pro kolonky; id, nick_name, email, password
+$dbName = "harnachp_login_register"; //jméno db, pak je users pro kolonky; id, nick_name, email, password; další kolonka pak je leaderboard - víc v todo.txt
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong:");
