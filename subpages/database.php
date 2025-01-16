d8be1b003c785c5d757ef0ab7d4c16587954e6df
Assasin238238 <harnach2006@gmail.com>
2025-01-16 08:48:39 +0100

    idk

--- subpages/database.php
+++ subpages/database.php
@@ -1,7 +1,7 @@
 <?php
 
 $hostName = "localhost";
-$dbUser = "root"; //max přepsat na jméno u db
+$dbUser = "root"; //max přepsat na jméno u db, u sebe to je root
 $dbPassword = ""; //přepsat když u vyšinky
 $dbName = "harnachp_login_register"; //jméno db, pak je users pro kolonky; id, nick_name, email, password; další kolonka pak je leaderboard - víc v todo.txt
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
